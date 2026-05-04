6e7c1c50a87c319bf5f8a8c64d9f8068a65c18d0
Yaksh Chauhan <yakshchauhan2@gmail.com>
2026-05-04 19:21:51 +0530

    Update Complaint.java

--- src/Complaint.java
+++ src/Complaint.java
@@ -1,1 +1,38 @@
+package oopsproject;
 
+public class Complaint {
+
+    private int ticketId;
+    private String issue;
+    private String status;
+
+    public Complaint() {
+        this.status = "Pending";
+    }
+
+    public Complaint(int ticketId, String issue) {
+        this.ticketId = ticketId;
+        this.issue = issue;
+        this.status = "Pending";
+    }
+
+    public void updateStatus(String newStatus) {
+        this.status = newStatus;
+    }
+
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public String getIssue() {
+        return issue;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String toString() {
+        return "Ticket : " + ticketId + " | Issue : " + issue + " | Status : " + status;
+    }
+}
